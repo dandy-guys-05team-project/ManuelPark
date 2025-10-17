951fb540cef3286cde711ea8474217bd044abb12
jazzyboi1991 <funkybot2002@gmail.com>
2025-10-17 09:49:06 +0900

    File Organised

---
+++ Java Spring Boot/Practice/firstproject/src/main/java/com/example/firstproject/FirstController.java
@@ -0,0 +1,20 @@
+package com.example.firstproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "Manuel");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNextTime(Model model) {
+        model.addAttribute("username", "Manny");
+        return "goodbye";
+    }
+}
